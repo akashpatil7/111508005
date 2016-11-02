14af98d8877c70244d50fd16aa1a6014f1610009
akashpatil7 <akash.aku97@gmail.com>
2016-11-02 21:28:46 +0530

    Update logic.c

--- logic.c
+++ logic.c
@@ -1,5 +1,5 @@
 /*************************************************************************
- Copyright (C) 2016 Adheesh Phadke adheesh1996@gmail.com
+ Copyright (C) 2016 Akash Patil patilap15.it@coep.ac.in
 
  This program is a free software, you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
